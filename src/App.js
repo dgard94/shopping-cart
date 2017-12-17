2bbccd37a4d62f7fa9ac459098a85973bc32a5c4
Hardiansa <ardititanium@gmail.com>
2017-12-17 22:56:08 +0700

    Added Component Needed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import ItemPage from "./components/ItemPage";
+import CartPage from "./components/CartPage";
+
+import "./index.css";
+import { items } from "./static-data";
+
 
 class App extends Component {
+
+  state = {
+    activeTab: 0,
+    cart: []
+  } 
+
+  handleTabChange = (index) => {
+    this.setState({
+      activeTab: index
+    })
+  }
+
+  handleAddToCart = (item) => {
+    this.setState({
+      cart: [...this.state.cart, item.id]
+    });
+  }
+
+  handleRemoveOne = (item) => {
+    let index = this.state.cart.indexOf(item.id);
+    this.setState({
+      cart:[
+        ...this.state.cart.slice(0, index),
+        ...this.state.cart.slice(index + 1)
+      ]
+    })
+  }
+
+  renderContent(){
+    switch(this.state.activeTab){
+      default:
+      case 0: return(
+          <ItemPage 
+            items={ items } 
+            onAddToCart={ this.handleAddToCart }/>
+        ); 
+      case 1: return this.renderCart();
+    }
+  }
+
+  renderCart(){
+    let itemsCounts = this.state.cart.reduce((itemsCounts, itemId) => {
+      itemsCounts[itemId] = itemsCounts[itemId] || 0;
+      itemsCounts[itemId]++;
+      return itemsCounts;
+    }, {});
+
+    let cartItems = Object.keys(itemsCounts).map(itemId => {
+      var item = items.find(item => 
+        item.id === parseInt(itemId, 10)
+      );
+
+      return {
+        ...item,
+        count: itemsCounts[itemId]
+      }
+    })
+
+
+    return(
+        <CartPage 
+          items={ cartItems } 
+          onAddOne={ this.handleAddToCart }
+          onRemoveOne={ this.handleRemoveOne } />
+      )
+  }
+
+
   render() {
+    let { activeTab } = this.state; 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav activeTab={ activeTab } onTabChange={ this.handleTabChange }/>
+        <main className="App-content">
+          {this.renderContent()}
+        </main>
       </div>
     );
   }
---
+++ src/static-data.js
@@ -0,0 +1,40 @@
+let items = [
+	{
+		id: 0,
+		name: "Apple iPad Mini 16GB",
+		description: "Apple iPad like no other. 16GB, WiFi, 4G.",
+		price: 299.00
+	},
+	{
+		id: 1,
+		name: "HP 20 C006L Desktop PC All In One",
+		description: "Processor : AMD E2-7110, Memori : 4 GB, Kapasitas penyimpanan : 500 GB",
+		price: 699.00
+	},
+	{
+		id: 2,
+		name: "MSI AE-201-i5 PC All In One Desktop PC",
+		description: "Prosesor : Intel® Core i5-4460S, Memori : DDR3 4GB, Max 16 GB, Kapasitas penyimpanan : 3.5 in SATA2 500 GB",
+		price: 720.00
+	},
+	{
+		id: 3,
+		name: "Logitech M90 Optical Mouse - Hitam",
+		description: "Memiliki teknologi scrolling side to side plus zoom yang akan membuat kerja Anda lebih mudah dan cepat",
+		price: 9.00
+	},
+	{
+		id: 4,
+		name: "WD My Passport New Design Portable Hard Disk Eksternal",
+		description: "Kapasitas : 1 TB, ard disk eksternal, Dilengkapi dengan software backup WD, ",
+		price: 56.00
+	},
+	{
+		id: 5,
+		name: "ASUS Notebook A456UR-GA091D Notebook",
+		description: "Layar : 14 Inch HD Color Shine (LED), Prosesor : Intel i5-7200U (3M Cache, 2.5GHz, up to 3.1GHz)",
+		price: 872.00
+	}
+];
+
+export { items };
